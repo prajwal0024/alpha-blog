ff6aeb1f344f81c6f683a10c9b7b76a70ca68ad2
prajwal0024 <prajwal0024@gmail.com>
2020-12-19 17:25:48 +0530

    Add password_digest column to user in backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true, 
                       uniqueness: { case_sensitive: false },
@@ -10,5 +11,7 @@ class User < ApplicationRecord
                 uniqueness: { case_sensitive: false }, 
                 length: { maximum: 125 }, 
                 format: { with: VALID_EMAIL_FORMAT }
+
+  has_secure_password
 
 end
